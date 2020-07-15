37411944f8812a8c96661e8c01fa775df8c91126
p4nash <nash.riaz1995@gmail.com>
2020-07-15 01:36:56 +0100

    Added base files
    Working R6 version

---
+++ Assets/Scripts/AimScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimScript : MonoBehaviour
+{
+    public bool collLeft, collRight, collUp, collDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collLeft = collRight = collUp = collDown = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Collision left with " + collision.gameObject.name);
+        if (collision.gameObject.name == "border left")
+        {
+            collLeft = false;
+        }
+        else if (collision.gameObject.name == "border right")
+        {
+            collRight = false;
+        }
+        else if (collision.gameObject.name == "border top")
+        {
+            collUp = false;
+        }
+        else if (collision.gameObject.name == "border bottom")
+        {
+            collDown = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision with " + collision.gameObject.name);
+        if(collision.gameObject.name == "border left")
+        {
+            collLeft = true;
+        }
+        else if (collision.gameObject.name == "border right")
+        {
+            collRight = true;
+        }
+        else if (collision.gameObject.name == "border top")
+        {
+            collUp = true;
+        }
+        else if (collision.gameObject.name == "border bottom")
+        {
+            collDown = true;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 leftMax, rightMax;
+    public Vector3 originalPosition;
+    public float maxMovement;
+
+    public string type; //0 means it spawned left, 1 means it spawned right
+
+    public float speed;
+
+    bool turnAround;
+
+    public bool Active;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftMax = transform.position - new Vector3(maxMovement, 0, 0);
+        rightMax = transform.position + new Vector3(maxMovement, 0, 0);
+        turnAround = false;
+    }
+
+    private void Awake()
+    {
+        //originalPosition = this.transform.position;
+        //Debug.Log("Original position is " + originalPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Active) return;
+
+        if(type == "left")
+        {
+            if(transform.position.x < rightMax.x && !turnAround)
+            {
+                transform.position += new Vector3(1, 0, 0) * speed;
+            }
+            else if(transform.position.x >= rightMax.x && !turnAround)
+            {
+                turnAround = true;
+            }
+            else if (turnAround && transform.position.x > originalPosition.x)
+            {
+                transform.position += new Vector3(-1, 0, 0) * speed;
+            }
+            else if (turnAround && transform.position.x <= originalPosition.x)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        else if (type == "right")
+        {
+            if (transform.position.x > leftMax.x && !turnAround)
+            {
+                transform.position += new Vector3(-1, 0, 0) * speed;
+            }
+            else if (transform.position.x <= leftMax.x && !turnAround)
+            {
+                turnAround = true;
+            }
+            else if (turnAround && transform.position.x < originalPosition.x)
+            {
+                transform.position += new Vector3(1, 0, 0) * speed;
+            }
+            else if(turnAround && transform.position.x >= originalPosition.x)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    public void Activate()
+    {
+        Debug.Log("Enemy activated");
+        Active = true;
+    }
+}
